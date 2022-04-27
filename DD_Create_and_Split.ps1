edf77fd7b19b7e42ad8672985324c3fc5d411564
braxtron <braxtron@gmail.com>
2022-04-27 12:09:29 -0600

    Update DD_Create_and_Split.ps1

--- DD_Create_and_Split.ps1
+++ DD_Create_and_Split.ps1
@@ -10,26 +10,23 @@ $Games = Get-ChildItem -Path 'Games' -Filter *.iso
 
 foreach($Game in $Games) {
     $GameWithPath = "Games\$Game"
-	if($Game.name.length -gt 36) {
-		$LongGame = $Game
+    if($Game.name.length -gt 36) {
+	$LongGame = $Game
         $ShortName = $LongGame.Name.Substring(0,36)
-		Rename-Item $GameWithPath $ShortName
+	Rename-Item $GameWithPath $ShortName
         $GameWithPath = "Games\$ShortName"
-		$Game = Get-Item $GameWithPath
-		Write-Host "$LongGame was renamed $Game"
-	}
+	$Game = Get-Item $GameWithPath
+	Write-Host "$LongGame was renamed $Game"
+    }
 	
     $GameDD = $GameWithPath.Substring(0,$GameWithPath.Length-4) + "_DD.iso"
-	$GameDDName = $GameDD.Substring(0,$GameDD.Length-4)
-	Write-Host "Game = $Game"
-	Write-Host "GameWithPath = $GameWithPath"
-	write-host "GameDD = $GameDD"
+    $GameDDName = $GameDD.Substring(0,$GameDD.Length-4)
 	
-    dd if=$GameWithPath of=$GameDD skip=387 bs=1M
- 	./fSplit.exe -split 4094 mb "$GameDD" -f "$GameDDName.{0}.iso"
+    ./dd.exe if=$GameWithPath of=$GameDD skip=387 bs=1M
+    ./fSplit.exe -split 4094 mb "$GameDD" -f "$GameDDName.{0}.iso"
 	
-	Remove-Item $GameDD
-	if($Yes.contains($DeleteSource)) {
-		Remove-Item -Recurse $GameWithPath
-	}
+    Remove-Item $GameDD
+    if($Yes.contains($DeleteSource)) {
+    	Remove-Item -Recurse $GameWithPath
+    }
 }
