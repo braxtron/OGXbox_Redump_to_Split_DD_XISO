f75f43f9563be5bdaad59864cf3790ba669e1310
Fredrik Hov <37329363+frehov@users.noreply.github.com>
2022-07-12 16:28:38 +0200

    Add expansion of zip archives
    Expands zip archives, overwriting any original file with the same name stored in the Games folder.

--- DD_Create_and_Split.ps1
+++ DD_Create_and_Split.ps1
@@ -1,22 +1,38 @@
 $DeleteSource = "a"
+$ExpandArchives = "a"
 $Yes = @("Yes", "YES", "yes", "Y", "y")
 $No = @("No", "NO", "no", "N", "n")
 
+while(-not($Yes.contains($ExpandArchives) -or $No.contains($ExpandArchives))) {
+	$ExpandArchives = Read-Host -Prompt "Find and expand ZIP archives to Games folder? [Y]es or [N]o"
+}
+
 while(-not($Yes.contains($DeleteSource) -or $No.contains($DeleteSource))) {
-	$DeleteSource = Read-Host -Prompt "Delete Source? [Y]es or [N]o"
+	$DeleteSource = Read-Host -Prompt "Delete Source ISOs after processing? [Y]es or [N]o"
+}
+
+if($Yes.contains($ExpandArchives)) {
+	$Zips = Get-ChildItem -Path 'Games' -Filter *.zip -Recurse
+
+	foreach($zip in $Zips) {
+		write-output "Trying to expand archive: $($zip.FullName)"
+		Expand-Archive -LiteralPath $zip.FullName -DestinationPath "Games" -Force
+	}
 }
 
 $Games = Get-ChildItem -Path 'Games' -Filter *.iso
 
 foreach($Game in $Games) {
+	write-output "Processing $Game"
 	$GameName = $Game.BaseName
 	if($GameName.length -gt 36) {
         $GameName = $Game.Name.Substring(0,36)
+		write-output "Truncating filename to: $GameName"
 	}
 	
     $GameDD = "Games\" + $GameName + "_DD.iso"
-    ./dd.exe if=Games\$Game of=$GameDD skip=1k bs=387k
- 	./fSplit.exe -split 4094 mb "$GameDD" -f "Games\$GameName.{0}.iso"
+    & "$PSScriptRoot\dd.exe" @("if=Games\$Game", "of=$GameDD", "skip=387", "bs=1M")
+ 	& "$PSScriptRoot\fSplit.exe"  @('-split', 4094, 'mb', $GameDD, '-df', 'Games', '-f', "$GameName.{0}.iso")
 	
 	Remove-Item $GameDD
 	if($Yes.contains($DeleteSource)) {
